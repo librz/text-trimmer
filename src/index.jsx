6e1fd9fb8370106d5332a9aa470e6c8b447079a0
rz <rz19960718@gmail.com>
2021-03-04 16:11:19 +0800

    chore

--- src/index.jsx
+++ src/index.jsx
@@ -4,9 +4,10 @@ import TextTrimmer from "./TextTrimmer";
 
 const Test = () => {
   return (
-    <div style={{ margin: 50, padding: 30, width: 500, border: "2px solid grey" }}>
+    <div style={{ margin: 50, padding: 30, border: "3px solid gray" }}>
+      <div style={{ marginBottom: 20 }}>Resize browser window to see the effect</div>
       <TextTrimmer
-        text="UTF-8 为什么能够兼容 ASCII? Emoji 的网络传输效率和普通文本不相上下？为什么 Microsoft Yahei 的字体文件远比 Helvetica 的要大的多? 看完这篇博客，获得所有答案"
+        text="Using props and state, we can put together a small Todo application. This example uses state to track the current list of items."
         onDetailsClick={text => alert(text)}
       />
     </div>
